af95552aa1a71cda9626be07434ab3925e2b0d87
sambad-K <sambadkhatiwada939@gmail.com>
2025-06-21 20:56:32 +0545

    Update dialogflow.js

--- Chatbot/dialogflow.js
+++ Chatbot/dialogflow.js
@@ -2,9 +2,9 @@ const dialogflow = require('@google-cloud/dialogflow');
 const path = require('path');
 
 
-process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(__dirname, 'sam-fdbo-c6616cf8001f.json');  
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(__dirname, 'path to api key json file');  
 
-const projectId = 'sam-fdbo';  
+const projectId = 'projct id';  
 const languageCode = 'en';
 
 const sessionClient = new dialogflow.SessionsClient();
